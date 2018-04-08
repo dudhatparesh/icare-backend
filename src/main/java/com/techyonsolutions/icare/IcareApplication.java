ada68c121912e7848463bca2efca736b863623c1
Paresh Dudhat <dudhatparesh@gmail.com>
2018-04-08 09:51:49 +0530

    initial code

---
+++ src/main/java/com/techyonsolutions/icare/IcareApplication.java
@@ -0,0 +1,12 @@
+package com.techyonsolutions.icare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IcareApplication.class, args);
+	}
+}
---
+++ src/test/java/com/techyonsolutions/icare/IcareApplicationTests.java
@@ -0,0 +1,16 @@
+package com.techyonsolutions.icare;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IcareApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
